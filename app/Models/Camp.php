7b56cf9cf01424ff7ac4a621f8771ebe10f7d3a1
estherak23 <70134970+estherak23@users.noreply.github.com>
2021-11-26 15:16:29 +0700

    migration camp,camp_benefit and checkout

---
+++ app/Models/Camp.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Facades\Auth;
+
+
+class Camp extends Model
+{
+    use HasFactory,SoftDeletes;
+
+    protected $fillable = [
+        'title',
+        'price',
+        //slug auto dari title jadi gak perlu diisi jadi gak dimasukin
+        ];
+
+        // public function getIsRegisteredAttribute()
+        // {
+        //     //kalo gak ada yg login returnya isregister false
+        //     if (!Auth::check())
+        //     {
+        //         return false;
+        //     }
+        //     //ambil id yg dipilih dan user id dari org yg login kalo ada return true kalo gak false
+        //     return Checkout::whereCampId($this->id)->whereUserId(Auth::id())->exists();
+        // }
+}
+
---
+++ app/Models/Checkout.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Checkout extends Model
+{
+    use HasFactory,SoftDeletes;
+
+    protected $fillable = [
+        'user_id','camp_id','card_number','expired','cvc','is_paid'
+        
+        
+    ];
+
+
+    public function setExpiredAttribute($value)
+    {
+        $this->attributes['expired'] = date('Y-m-t', strtotime($value));
+    }
+
+// untuk relasi untuk ambil2 datanya
+    public function Camp(): BelongsTo
+    {
+        return $this->belongsTo(Camp::class);
+    }
+
+    public function User(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
