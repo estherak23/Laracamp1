893d10c006447c49dc55ce003afcaaef253c7e20
estherak23 <70134970+estherak23@users.noreply.github.com>
2021-11-26 16:04:01 +0700

    seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,5 +14,11 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        //panggil seeder yg udah dibuat
+        $this->call([
+            CampSeeder::class,
+            CampBenefitTableSeeder::class,
+            AdminUserSeeder::class
+        ]);
     }
 }
